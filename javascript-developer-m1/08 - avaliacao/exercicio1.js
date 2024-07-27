8efcaf84caae0c1f1b0c77ede3da652d6f982ec0
yyhago <yhago.felipe.teles@gmail.com>
2024-07-27 01:24:09 -0300

    Commit Doc.Aula

--- javascript-developer-m1/08 - avaliacao/exercicio1.js
+++ javascript-developer-m1/08 - avaliacao/exercicio1.js
@@ -1,4 +1,4 @@
-// 1) Faça um programa que receba a média de um aluno.
+// 1) Faça um programa que receba a média de um aluno!
 // Caso a média seja < 5 imprima "Reprovado"
 // Caso a média seja >= 5 e < 7 imprima "Recuperação"
 // Caso a média seja >= 7 imprima "Aprovado"
