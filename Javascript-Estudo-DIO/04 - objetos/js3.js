9e9929601b47c8e2b67c40fc581f721d416b96e8
yyhago <yhago.felipe.teles@gmail.com>
2024-07-27 22:44:13 -0300

    Lição POO Finalizada v2

--- Javascript-Estudo-DIO/04 - objetos/js3.js
+++ Javascript-Estudo-DIO/04 - objetos/js3.js
@@ -1,31 +1,35 @@
-/**
-  2) Crie uma classe para representar pessoas.
-  Para cada pessoa teremos os atributos nome, peso e altura.
-  As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
-  Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
-  do seu IMC;
- */
-
-// Definição da classe Pessoas.
-class Pessoas{
-  nome;
-  peso;
-  altura
-
-  // Constructor da classe
-  constructor(nome, peso, altura){
+// Definição da classe Pessoa
+class Pessoa {
+  constructor(nome, peso, altura) {
     this.nome = nome;
     this.peso = peso;
     this.altura = altura;
   }
 
   // Método para realização do cálculo do IMC
-  calcularIMC(peso,altura){
+  calcularIMC() {
     return this.peso / (this.altura * this.altura);
   }
 
+  // Método para classificação dos IMC's
+  classificarImc() {
+    const imc = this.calcularIMC();
+    if (imc < 18.5) {
+      return 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc < 25) {
+      return 'Peso normal';
+    } else if (imc >= 25 && imc < 30) {
+      return 'Acima do peso';
+    } else if (imc >= 30 && imc < 40) {
+      return 'Obeso';
+    } else {
+      return 'Obesidade Grave';
+    }
+  }
 }
 
-// Instancia do josé com a saida dos valores e aprensentação do IMC
-const jose = new Pessoas('José', 70, 1.75)
-console.log(`O IMC de José é: ${jose.calcularIMC(70, 1.75).toFixed(2)}`)
+// Instancia do José com a saída dos valores e apresentação do IMC
+const jose = new Pessoa('José', 70, 1.75);
+const imc = jose.calcularIMC().toFixed(2);
+const classificacao = jose.classificarImc();
+console.log(`O IMC de José é: ${imc} (${classificacao})`);
