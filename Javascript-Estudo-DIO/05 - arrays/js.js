e5550b3253623cb7243a7e565ab5ebdf4385651c
yyhago <yhago.felipe.teles@gmail.com>
2024-07-29 15:49:45 -0300

    Exercicios de Arrays

---
+++ Javascript-Estudo-DIO/05 - arrays/js.js
@@ -0,0 +1,11 @@
+const notasAlunos = []
+
+notasAlunos.push(8)
+notasAlunos.push(6)
+notasAlunos.push(10)
+notasAlunos.push(5)
+notasAlunos.push(8)
+
+const soma = notasAlunos[0] + notasAlunos[1] + notasAlunos[2] + notasAlunos[3] + notasAlunos[4]
+
+console.log(soma / notasAlunos.length)
